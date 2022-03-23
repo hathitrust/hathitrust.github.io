aaaeea11c004e28e57647dc4c341803b8f16bad5
Aaron Elkiss <aelkiss@umich.edu>
2022-03-23 09:03:19 -0400

    Add packages for grok 9.7.4, smarty 2.6.31, netpbm 10.86.31

---
+++ debian/update_debs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Run this script to regenerate the Packages/Release file when you add or
+# update debs in the repository (/var/www/html/debs)
+
+RELEASE=$1
+# SIGN_KEY=$(SIGN_KEY|-"~/.gnupg/deb-signing.key")
+DEBHOME=$(dirname $(realpath $0))
+
+if [[ -z "$RELEASE" ]];
+then echo "Usage: $0 release (e.g. bullseye)";
+          exit 1;
+fi
+
+# echo "Importing key (if necessary) and signing."
+
+# gpg --import $SIGN_KEY
+
+CONFIG=$(cat <<EOT
+APT {
+  FTPArchive {
+    Release {
+      Origin "HathiTrust";
+      Label "HathiTrust";
+      Codename "$RELEASE";
+      Components "main";
+    };
+  };
+};
+EOT
+)
+
+cd $DEBHOME
+dpkg-scanpackages dists/$RELEASE/main/binary-amd64 > dists/$RELEASE/main/binary-amd64/Packages
+dpkg-scanpackages dists/$RELEASE/main/binary-all > dists/$RELEASE/main/binary-all/Packages
+apt-ftparchive -c <(echo "$CONFIG") release dists/$RELEASE > dists/$RELEASE/Release
+
+cd dists/$RELEASE
+gpg -abs -o Release.gpg Release
+gpg --clearsign --output  InRelease Release
+
